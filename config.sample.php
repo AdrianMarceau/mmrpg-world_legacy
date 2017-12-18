03b29e89dab12b45ac119e1c0167407c2b1872ba
AdrianMarceau <adrian.marceau@gmail.com>
2017-12-18 18:26:23 -0500

    Updated sample config with DB details.

--- config.sample.php
+++ config.sample.php
@@ -4,4 +4,11 @@
 $mmrpg_root_dir = '/var/www/html/';
 $mmrpg_root_url = 'http://www.domain.com/';
 
+// Define database connection details
+$mmrpg_database_host = 'localhost';
+$mmrpg_database_name = 'legacy_table';
+$mmrpg_database_username = 'legacy_user';
+$mmrpg_database_password = 'P@SSW0RD';
+$mmrpg_database_charset = 'utf8';
+
 ?>
