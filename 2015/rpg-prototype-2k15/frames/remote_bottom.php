87208d9c33b8d3f85ecdb09f75eef87b72f70c08
AdrianMarceau <adrian.marceau@gmail.com>
2018-01-06 13:53:40 -0500

    Removing 2k15 build from this repo in order to split off into new sub-repo.

--- 2015/rpg-prototype-2k15/frames/remote_bottom.php
+++
@@ -1,9 +0,0 @@
-<?
-// If this was a script request, immediately kill the session data
-if (defined('MMRPG_REMOTE_GAME')){
-  $temp_session_key = 'REMOTE_GAME_'.MMRPG_REMOTE_GAME;
-  unset($_SESSION['RPG2k15'][$temp_session_key]);
-}
-// Unset the database variable
-unset($DB);
-?>
